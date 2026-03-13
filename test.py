7e441fa6d51260473091b2e2e723158b9342abe4
transportboyer <endlessriver2024@163.com>
2026-03-13 11:58:07 +0800

    add test

---
+++ test.py
@@ -0,0 +1,5 @@
+# Install opencode-plugin-openspec by following the instructions here: https://raw.githubusercontent.com/Octane0411/opencode-plugin-openspec/main/README.md
+
+git config --global user.name "transportboyer"
+git config --global user.email "endlessriver2024@163.com"
+   
